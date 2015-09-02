a79a7bf435bc5ef8ff39a5bf70d4d4644da27dbd
Michael W Powell <mwpowellhtx@gmail.com>
2015-09-02 08:10:25 -0400

    added clarifying comments

--- src/Kingdom.Unitworks.Core/Dimensions/IMass.cs
+++ src/Kingdom.Unitworks.Core/Dimensions/IMass.cs
@@ -1,8 +1,14 @@
 namespace Kingdom.Unitworks.Dimensions
 {
     /// <summary>
-    /// 
+    /// In physics, Mass is a property of a physical body which determines the strength of its
+    /// mutual gravitational attraction to other bodies, its resistance to being
+    /// <see cref="IAcceleration">accelerated</see> by a <see cref="IForce">force</see>, and in
+    /// the theory of relativity gives the mass–energy content of a system. The SI unit of mass
+    /// is the <see cref="Systems.SI.Mass.Kilogram"/>.
     /// </summary>
+    /// <a href="!:http://en.wikipedia.org/wiki/Mass" > Mass </a>
+    /// <a href="!:http://en.wikipedia.org/wiki/Weight" > Weight </a>
     public interface IMass : IBaseDimension
     {
     }
